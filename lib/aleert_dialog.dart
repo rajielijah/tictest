ecf8d5cc30c2fd49ba96d6b18fd531a453cb5cbb
Raji Elijah <rajielijah@gmail.com>
2022-03-22 14:13:50 +0100

    build the alert dialog box

--- lib/aleert_dialog.dart
+++ lib/aleert_dialog.dart
@@ -5,21 +5,21 @@ import 'package:flutter/material.dart';
 
 Future<void> showAlertDialog({
   required BuildContext context,
-  @required String? title,
-  @required String? content,
-  @required String? defaultActionText,
-  final VoidCallback? onOkPressed
+  required String title,
+  required String content,
+  required String defaultActionText,
+  required final VoidCallback onOkPressed
 }) async {
   if (Platform.isIOS) {
     return await showCupertinoDialog(
       context: context,
       builder: (context) => CupertinoAlertDialog(
-        title: Text(title!),
-        content: Text(content!),
+        title: Text(title),
+        content: Text(content),
         actions: <Widget>[
           CupertinoDialogAction(
-            onPressed: () => onOkPressed!(),
-            child: Text(defaultActionText!),
+            onPressed: () => onOkPressed(),
+            child: Text(defaultActionText),
           ),
         ],
       ),
@@ -29,12 +29,12 @@ Future<void> showAlertDialog({
   return await showDialog(
     context: context,
     builder: (context) => AlertDialog(
-      title: Text(title!),
-      content: Text(content!),
+      title: Text(title),
+      content: Text(content),
       actions: <Widget>[
         FlatButton(
-          onPressed: () => onOkPressed!(),
-          child: Text(defaultActionText!),
+          onPressed: () => onOkPressed(),
+          child: Text(defaultActionText),
         ),
       ],
     ),
